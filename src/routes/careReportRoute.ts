6f72d25cb59d618798c8ae172c573704e06f6b56
JinseonYe <aatwe24@gmail.com>
2025-02-11 11:35:18 +0900

    [Refactor] api 자원 명 카멜케이스에서 케밥케이스로 변경

--- src/routes/careReportRoute.ts
+++ src/routes/careReportRoute.ts
@@ -3,8 +3,8 @@ import * as careReportController from '../controllers/careReportController';
 
 const router = express.Router();
 
-router.post('/api/careReport', careReportController.createCareReport);
-router.get('/api/careStatus', careReportController.getAllCareStatus);
-router.get('/api/careReport', careReportController.getAllCareReport);
+router.post('/api/care-report', careReportController.createCareReport);
+router.get('/api/care-status', careReportController.getAllCareStatus);
+router.get('/api/care-report', careReportController.getAllCareReport);
 
 export default router;
